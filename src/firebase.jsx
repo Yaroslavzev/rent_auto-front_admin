20d64a3f02e285f05cbfd579aa29c2047e5823b8
Edinbox2 <edinbox2@mail.ru>
2018-11-15 22:38:11 +0800

    auth

---
+++ src/firebase.jsx
@@ -0,0 +1,21 @@
+import firebase from 'firebase/app'; 
+import 'firebase/app';
+import 'firebase/database'; 
+import 'firebase/auth'; 
+
+const config={
+    apiKey: "AIzaSyA14dDnNeK8niqLK4xA1LKKxwvTXyVxmxI",
+    authDomain: "rentadmin-8034e.firebaseapp.com",
+    databaseURL: "https://rentadmin-8034e.firebaseio.com",
+    projectId: "rentadmin-8034e",
+    storageBucket: "rentadmin-8034e.appspot.com",
+    messagingSenderId: "470383057131"
+}
+
+firebase.initializeApp(config);
+
+const firebaseDB = firebase.database();
+
+export {
+    firebase, 
+}
