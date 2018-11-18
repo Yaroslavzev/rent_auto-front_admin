5c1f5c2dc8bef527d31191430494dd1d8824405b
Edinbox2 <edinbox2@mail.ru>
2018-11-19 00:38:15 +0800

    сделал таблицу автомобилей в админке

---
+++ src/components/admin/cars/index.jsx
@@ -0,0 +1,67 @@
+import React, { Component } from "react";
+import AdminLayout from "../../hoc/adminLayout";
+import { Link } from "react-router-dom";
+import "./cars.css";
+import axios from "axios";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import CircularProgress from "@material-ui/core/CircularProgress";
+class Cars extends Component {
+  state = {
+    isLoading: true,
+    cars: []
+  };
+
+  componentDidMount() {
+    axios.get(`https://api.rent-auto.biz.tm/models`).then(res => {
+      const list = res.data;
+      this.setState({ cars: list, isLoading: false });
+    });
+  }
+  render() {
+    console.log(this.state.cars);
+    return (
+      <AdminLayout>
+        <div>
+          <Paper>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>id</TableCell>
+                  <TableCell>name</TableCell>
+                  <TableCell>brand</TableCell>
+                  <TableCell>note</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {this.state.cars.map((item, i) => (
+                  <TableRow key={i}>
+                    <TableCell>{item.id}</TableCell>
+                    <TableCell>
+                      <Link to={`/dashboard/cars/${item.id}`}>{item.name}</Link>
+                    </TableCell>
+                    <TableCell>{item.code}</TableCell>
+                    <TableCell>{item.note}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </Paper>
+          <div className="progress_cars">
+            {this.state.isLoading ? (
+              <CircularProgress thikness={5} style={{ color: "lightblue" }} />
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
+      </AdminLayout>
+    );
+  }
+}
+
+export default Cars;
--- src/routes.jsx
+++ src/routes.jsx
@@ -1,14 +1,18 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
 import SignIn from "./components/signIn/index";
 import Dashboard from "./components/admin/dashboard";
 import PrivateRoute from './components/authRoutes/privateRoute';
 import PublicRoute from './components/authRoutes/publicRoute';
+import Cars from './components/admin/cars';
+import CarInfo from './components/admin/cars/carInfo';
 
 const Routes = (props) => {
   return (
     <div>
       <Switch>
+      <PrivateRoute {...props} path="/dashboard/cars/:id" exact component={CarInfo}/>
+      <PrivateRoute {...props} path="/dashboard/cars" exact component={Cars}/>
         <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
         <PublicRoute {...props} path="/sign_in" restricted={true} exact component={SignIn} />
       </Switch>
