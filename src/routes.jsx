45286b4afe1522ebd3af7aae02d19b713cbef0d4
Edinbox2 <edinbox2@mail.ru>
2018-11-19 21:43:01 +0800

    сделал redirect из "/"

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch } from "react-router-dom";
+import { Switch, Redirect } from "react-router-dom";
 import SignIn from "./components/signIn/index";
 import Dashboard from "./components/admin/dashboard";
 import PrivateRoute from './components/authRoutes/privateRoute';
@@ -10,9 +10,10 @@ import CarInfo from './components/admin/cars/carInfo';
 const Routes = (props) => {
   return (
     <div>
-      <Switch>
-      <PrivateRoute {...props} path="/dashboard/cars/:id" exact component={CarInfo}/>
-      <PrivateRoute {...props} path="/dashboard/cars" exact component={Cars}/>
+      <Redirect from="/" to={props.user ? '/dashboard' : '/sign_in'}/>
+      <Switch>  
+        <PrivateRoute {...props} path="/dashboard/cars/:id" exact component={CarInfo}/>
+        <PrivateRoute {...props} path="/dashboard/cars" exact component={Cars}/>
         <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
         <PublicRoute {...props} path="/sign_in" restricted={true} exact component={SignIn} />
       </Switch>
