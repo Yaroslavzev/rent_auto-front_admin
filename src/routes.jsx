5f37e9074f181aed00e41440622ed66cf3b512c3
Edinbox2 <edinbox2@mail.ru>
2018-11-20 13:35:53 +0800

    изменил переадресацтю из /

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Redirect } from "react-router-dom";
+import { Switch, Redirect, Route } from "react-router-dom";
 import SignIn from "./components/signIn/index";
 import Dashboard from "./components/admin/dashboard";
 import PrivateRoute from './components/authRoutes/privateRoute';
@@ -10,12 +10,12 @@ import CarInfo from './components/admin/cars/carInfo';
 const Routes = (props) => {
   return (
     <div>
-      <Redirect from="/" to={props.user ? '/dashboard' : '/sign_in'}/>
       <Switch>  
         <PrivateRoute {...props} path="/dashboard/cars/:id" exact component={CarInfo}/>
         <PrivateRoute {...props} path="/dashboard/cars" exact component={Cars}/>
         <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
         <PublicRoute {...props} path="/sign_in" restricted={true} exact component={SignIn} />
+        {  <Route path="/" exact /> ? <Redirect to={props.user ? '/dashboard' : '/sign_in'}/> : null }  
       </Switch>
     </div>
   );
